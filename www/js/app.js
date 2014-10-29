18efc1951d9f7cc138f5bc31e8eb6860546bd0cf
looklikeapro <jerryzhou00@gmail.com>
2014-10-28 20:35:39 -0400

    12

--- www/js/app.js
+++ www/js/app.js
@@ -5,155 +5,202 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ionic.contrib.ui.cards'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.directive('noScroll', function($document) {
+    .directive('noScroll', function ($document) {
 
-  return {
-    restrict: 'A',
-    link: function($scope, $element, $attr) {
+        return {
+            restrict: 'A',
+            link: function ($scope, $element, $attr) {
 
-      $document.on('touchmove', function(e) {
-        e.preventDefault();
-      });
-    }
-  }
-})
+                $document.on('touchmove', function (e) {
+                    e.preventDefault();
+                });
+            }
+        }
+    })
 
 
-.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate, $ionicSideMenuDelegate, $ionicModal) {
+    .controller('CardsCtrl', function ($scope, $ionicSwipeCardDelegate, $ionicSideMenuDelegate, $ionicModal, $ionicActionSheet, $timeout) {
 
-  $scope.items = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 11 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-    { id: 21 },
-    { id: 22 },
-    { id: 23 },
-    { id: 24 },
-    { id: 25 },
-    { id: 26 },
-    { id: 27 },
-    { id: 28 },
-    { id: 29 },
-    { id: 30 },
-    { id: 31 },
-    { id: 32 },
-    { id: 33 },
-    { id: 34 },
-    { id: 35 },
-    { id: 36 },
-    { id: 37 },
-    { id: 38 },
-    { id: 39 },
-    { id: 40 },
-    { id: 41 },
-    { id: 42 },
-    { id: 43 },
-    { id: 44 },
-    { id: 45 },
-    { id: 46 },
-    { id: 47 },
-    { id: 48 },
-    { id: 49 },
-    { id: 50 }
-  ];
-  var cardTypes = [{
-    title: 'Swipe down to clear the card',
-    image: 'img/pic.png'
-  }, {
-    title: 'Where is this?',
-    image: 'img/pic.png'
-  }, {
-    title: 'What kind of grass is this?',
-    image: 'img/pic2.png'
-  }, {
-    title: 'What beach is this?',
-    image: 'img/pic3.png'
-  }, {
-    title: 'What kind of clouds are these?',
-    image: 'img/pic4.png'
-  }];
+        $scope.items = [
+            { id: 0 },
+            { id: 1 },
+            { id: 2 },
+            { id: 3 },
+            { id: 4 },
+            { id: 5 },
+            { id: 6 },
+            { id: 7 },
+            { id: 8 },
+            { id: 9 },
+            { id: 10 },
+            { id: 11 },
+            { id: 12 },
+            { id: 13 },
+            { id: 14 },
+            { id: 15 },
+            { id: 16 },
+            { id: 17 },
+            { id: 18 },
+            { id: 19 },
+            { id: 20 },
+            { id: 21 },
+            { id: 22 },
+            { id: 23 },
+            { id: 24 },
+            { id: 25 },
+            { id: 26 },
+            { id: 27 },
+            { id: 28 },
+            { id: 29 },
+            { id: 30 },
+            { id: 31 },
+            { id: 32 },
+            { id: 33 },
+            { id: 34 },
+            { id: 35 },
+            { id: 36 },
+            { id: 37 },
+            { id: 38 },
+            { id: 39 },
+            { id: 40 },
+            { id: 41 },
+            { id: 42 },
+            { id: 43 },
+            { id: 44 },
+            { id: 45 },
+            { id: 46 },
+            { id: 47 },
+            { id: 48 },
+            { id: 49 },
+            { id: 50 }
+        ];
+        var cardTypes = [
+            {
+                title: 'Swipe down to clear the card',
+                image: 'img/pic.png'
+            },
+            {
+                title: 'Where is this?',
+                image: 'img/pic.png'
+            },
+            {
+                title: 'What kind of grass is this?',
+                image: 'img/pic2.png'
+            },
+            {
+                title: 'What beach is this?',
+                image: 'img/pic3.png'
+            },
+            {
+                title: 'What kind of clouds are these?',
+                image: 'img/pic4.png'
+            }
+        ];
 
-  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+        $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
 
-  $scope.cardSwiped = function(index) {
-    $scope.addCard();
-  };
-
-  $scope.cardDestroyed = function(index) {
-    $scope.cards.splice(index, 1);
-  };
-
-  $scope.addCard = function() {
-    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-    newCard.id = Math.random();
-    $scope.cards.push(angular.extend({}, newCard));
-  }
-
-  $ionicModal.fromTemplateUrl('templates/new-card.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
-  });
-
-        $scope.createCard=function(card){
-            cards.push(card);
+        $scope.cardSwiped = function (index) {
+            $scope.addCard();
         };
-})
 
-.controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
-  $scope.goAway = function() {
-    var card = $ionicSwipeCardDelegate.getSwipebleCard($scope);
-    card.swipe();
-  };
-});
+        $scope.cardDestroyed = function (index) {
+            $scope.cards.splice(index, 1);
+        };
+
+        $scope.addCard = function () {
+            var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+            newCard.id = Math.random();
+            $scope.cards.push(angular.extend({}, newCard));
+        }
+
+        $ionicModal.fromTemplateUrl('templates/new-card.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function () {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function () {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function () {
+            // Execute action
+        });
+
+        $scope.createCard = function (card) {
+            cards.push(card);
+            alert("lol");
+        };
+
+        $scope.show = function() {
+
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    { text: '<b>New</b>' },
+                    { text: 'Edit' },
+                    { text: 'Move' },
+                    { text: 'Hide All' }
+                ],
+                destructiveText: 'Delete',
+                titleText: 'Edit',
+                cancelText: 'Cancel',
+                cancel: function() {
+                    // add cancel code..
+                },
+                buttonClicked: function(index) {
+                    if (index==0){
+                        $scope.openModal();
+                    }
+                    else if (index == 1){
+                        alert("0");
+                    }
+                    else if (index == 2){
+                        alert("0");
+                    }
+                    else if (index == 3){
+                        alert("0");
+                    }
+                    return true;
+                }
+            });
+
+            // For example's sake, hide the sheet after two seconds
+            $timeout(function() {
+                hideSheet();
+            }, 4000);
+
+        };
+    })
+
+    .controller('CardCtrl', function ($scope, $ionicSwipeCardDelegate) {
+        $scope.goAway = function () {
+            var card = $ionicSwipeCardDelegate.getSwipebleCard($scope);
+            card.swipe();
+        };
+    });
 

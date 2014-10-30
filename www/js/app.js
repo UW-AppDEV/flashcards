4ebdb663fa0533c29a8cb8e8a02067f7feacfcb0
looklikeapro <jerryzhou00@gmail.com>
2014-10-29 21:33:40 -0400

    somestuff

--- www/js/app.js
+++ www/js/app.js
@@ -38,13 +38,7 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.cards'])
             { id: 1 },
             { id: 2 },
             { id: 3 },
-            { id: 4 },
-            { id: 5 },
-            { id: 6 },
-            { id: 7 },
-            { id: 8 },
-            { id: 9 },
-            { id: 10 }
+            { id: 4 }
         ];
         var cardTypes = [
             {

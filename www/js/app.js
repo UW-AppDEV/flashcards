2792461db2d0abbbf5acd5a2a10ac01771c3c3a2
looklikeapro <jerryzhou00@gmail.com>
2014-10-31 21:21:58 -0400

    data structure

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,3 @@
-angular.module('ionic.utils', [])
-    .factory('$localstorage', ['$window', function ($window) {
-        return {
-            set: function (key, value) {
-                $window.localStorage[key] = value;
-            },
-            get: function (key, defaultValue) {
-                return $window.localStorage[key] || defaultValue;
-            },
-            setObject: function (key, value) {
-                $window.localStorage[key] = JSON.stringify(value);
-            },
-            getObject: function (key, defaultValue) {
-                return JSON.parse($window.localStorage[key] || JSON.stringify(defaultValue));
-            }
-        }
-    }]);
 angular.module('starter', ['ionic', 'ionic.contrib.ui.cards', 'ionic.utils'])
 
     .run(function ($ionicPlatform, $localstorage) {
